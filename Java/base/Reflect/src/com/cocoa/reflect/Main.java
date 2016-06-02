9ba506e796def2ef320dcf0bbe8b8a957bb9dd15
shenjun <shenjun@kuaiqiangche.com>
2016-06-02 16:06:19 +0800

    add test

---
+++ Java/base/Reflect/src/com/cocoa/reflect/Main.java
@@ -0,0 +1,14 @@
+package com.cocoa.reflect;
+
+public class Main{
+
+      public static void main(String[] args){
+
+             Class  mainClass =  Main.class;
+
+            //
+            // System.out.print(String.format("name is%s",mainClass.getName()));
+
+
+      }
+}
