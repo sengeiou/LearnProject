f2fea793f619c95757440909e1633308fde8da26
cocoa <cocoooooa@gmail.com>
2016-07-15 21:33:11 +0800

    add method

--- go/base/method.go
+++ go/base/method.go
@@ -4,6 +4,28 @@ import (
 	"fmt"
 )
 
-func main() {
+type A struct {
+	Name string
+}
 
+/**
+1 方法就是有接收者的函数 （a A）为接收者   func (a A) Print() {
+2 接收者不能定义为接口类型
+3 接收者类型只能是 T  或  *T ，（T是类型）
+*/
+func main() {
+	a := A{}
+	a.Print()
+
+	b := 1
+	//  b.Test()    这样写是错误的
+
+}
+
+func (a A) Print() {
+	fmt.Println("-------------Print----------")
+}
+
+func (a int) Test() {
+	fmt.Println("-------------Test----------")
 }
