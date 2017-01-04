1b4ac95443755648b366b8900ab455e7f3857bec
cococa <cocoooooa@gmail.com>
2017-01-04 23:16:01 +0800

    123

--- Java/Test/ThreadLocalTest.java
+++ Java/Test/ThreadLocalTest.java
@@ -1,9 +1,12 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class ThreadLocalTest extends Thread {
 
 
 	public static ThreadLocal<String> threadLocal = new ThreadLocal<String>() {
 
-		@Override 
+		@Override
 		protected String initialValue() {
 			// 设置初始值，这里使用当前thread的名字
 			return Thread.currentThread().getName();
@@ -14,14 +17,28 @@ public class ThreadLocalTest extends Thread {
 	public static void main(String[] args) {
 
 
-		// threadLocal.set("main");
+		try {
+			String DateStr1 = "2017-12-12";
 
-		ThreadLocalTest test = new ThreadLocalTest();
-		test.start();//在子线程中获取
+			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+			Date dateTime1 = dateFormat.parse(DateStr1);
+			Date dateTime2 = dateFormat.parse(dateFormat.format(new Date()));
+			int i = dateTime1.compareTo(dateTime2);
+			System.out.println(i);
+		} catch (Exception e) {
 
-		//在主线程中获取
-		String result = threadLocal.get();
-		System.out.println("main thread get str" + result);
+		}
+
+
+
+		// // threadLocal.set("main");
+
+		// ThreadLocalTest test = new ThreadLocalTest();
+		// test.start();//在子线程中获取
+
+		// //在主线程中获取
+		// String result = threadLocal.get();
+		// System.out.println("main thread get str" + result);
 	}
 
 
