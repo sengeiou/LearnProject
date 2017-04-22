851221ddafe297aa16ddc574e7fea23a4185fe0a
cococa <cocoooooa@gmail.com>
2017-04-22 10:37:29 +0800

    ad

---
+++ iOS/base/ForTest.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+// just play , have fun , enjoy the game
+
+import UIKit
+
+
+var str = "Hello, playground"
+
+
+print("haha")
+
+var label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 40))
+label.textAlignment = .center
+label.text = "立即购买"
+label.backgroundColor = UIColor.orange
+label.textColor  = UIColor.white
