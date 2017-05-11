cf6b0a0e57c917e9774e8f4687b2444b59b681b8
cococa <cocoooooa@gmail.com>
2017-05-11 11:32:46 +0800

    add

--- iOS/base/TestGCD/TestGCD/ViewController.swift
+++ iOS/base/TestGCD/TestGCD/ViewController.swift
@@ -21,27 +21,53 @@ class ViewController: UIViewController {
        // Thread.sleep(forTimeInterval: 5)
         
         let queue  = OperationQueue()
-        queue.addOperation {
+//        queue.addOperation {
+//            
+//            // at child thread
+//            Thread.sleep(forTimeInterval: 5)
+//            let text  = "changed"
+//            OperationQueue.main.addOperation{
+//                print(text)
+//            }
+//            
+//        }
+        
+        let thread1 = BlockOperation{
+            print("thread1 start")
+            Thread.sleep(forTimeInterval: 3)
             
-            // at child thread
-            Thread.sleep(forTimeInterval: 5)
-            let text  = "changed"
-            OperationQueue.main.addOperation{
-                print(text)
-            }
-            
+            OperationQueue.main.addOperation({
+                print("thread1 ok")
+            })
         }
+        thread1.completionBlock = {
+            print("thread1 is complete")
+        }
+        
+        let thread2  = BlockOperation{
+            print("thread2 start")
+            Thread.sleep(forTimeInterval: 3)
+            OperationQueue.main.addOperation({
+                print("thread2 ok")
+            })
+        }
+        
+        thread2.addDependency(thread1)
+        
+        queue.addOperation(thread1)
+        queue.addOperation(thread2)
+        
+        
+        // cancel task
+//        Thread.sleep(forTimeInterval: 1)
+//        queue.cancelAllOperations()
+        
+
     }
-    
-    
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    
-    
-
 }
 
