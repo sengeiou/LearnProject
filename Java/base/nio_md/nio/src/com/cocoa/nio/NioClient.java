500b8c228860b665ceb19f293284d900c6708d34
cococa <cocoooooa@gmail.com>
2017-06-18 20:17:35 +0800

    add java nio

--- Java/base/nio_md/nio/src/com/cocoa/nio/NioClient.java
+++ Java/base/nio_md/nio/src/com/cocoa/nio/NioClient.java
@@ -5,26 +5,97 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Set;
 
 /**
  * Created by cocoa on 2017/6/14.
  */
 public class NioClient extends Main {
 
+
+    static ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+    static ByteBuffer writeBuffer = ByteBuffer.allocate(1024);
+    private static SocketChannel ss;
+    private static Selector selector;
+
+
     public static void main(String[] args) {
         try {
-            Socket socket = new Socket(ADDRESS, PORT);
-            OutputStream outputStream = socket.getOutputStream();
-            OutputStreamWriter bufferedOutputStream = new OutputStreamWriter(outputStream);
-            bufferedOutputStream.write("Hello nio server");
-            bufferedOutputStream.close();
-            outputStream.close();
-            socket.close();
+            ss = SocketChannel.open();
+//            ss.socket().bind(new InetSocketAddress(PORT));
+            ss.configureBlocking(false);
+            ss.connect(new InetSocketAddress(ADDRESS,PORT));
+            selector = Selector.open();
+            ss.register(selector, SelectionKey.OP_CONNECT);
 
+            if (ss.isConnectionPending()) {
+                ss.finishConnect();
+                ss.configureBlocking(false);
+                ss.register(selector, SelectionKey.OP_WRITE);
+            }
+
+
+            Scanner scanner = new Scanner(System.in);
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+
+                if ("exit".equals(line)) {
+                    break;
+                }
+                process(line);
+            }
 
         } catch (Exception e) {
             System.out.println(e.toString());
         }
+    }
+
+
+    public static void process(String msg) throws Exception {
+        while (true) {
+            int t = selector.select();
+            if (t == 0) {
+                continue;
+            }
+            Set<SelectionKey> keySet = selector.selectedKeys();
+            Iterator<SelectionKey> selectionKeyIterator = keySet.iterator();
+
+            while (selectionKeyIterator.hasNext()) {
+                SelectionKey key = selectionKeyIterator.next();
+//                    if (key.isConnectable()) {
+//                        ss.finishConnect();
+//                        ss.configureBlocking(false);
+//                        ss.register(selector, SelectionKey.OP_WRITE);
+//                    } else
+                if (key.isReadable()) {
+                    SocketChannel socketChannel = (SocketChannel) key.channel();
+                    readBuffer.clear();
+                    socketChannel.read(readBuffer);
+                    readBuffer.flip();
+                    while (readBuffer.hasRemaining()) {
+                        System.out.print(readBuffer.get());
+                    }
+                    ss.register(selector, SelectionKey.OP_WRITE);
+
+                } else if (key.isWritable()) {
+                    SocketChannel socketChannel = (SocketChannel) key.channel();
+                    writeBuffer.clear();
+                    writeBuffer.put(msg.getBytes());
+                    socketChannel.write(writeBuffer);
+                    ss.register(selector, SelectionKey.OP_READ);
+
+                }
+                selectionKeyIterator.remove();
+            }
+        }
+
+
     }
 
 }
--- Java/base/nio_md/nio/src/com/cocoa/nio/NioServer.java
+++ Java/base/nio_md/nio/src/com/cocoa/nio/NioServer.java
@@ -18,48 +18,55 @@ public class NioServer extends Main {
 
     public static ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
 
-    public static void main(String[] args) {
-        try {
-            ServerSocketChannel ssc = ServerSocketChannel.open();
-            ssc.bind(new InetSocketAddress(ADDRESS, PORT));
-            ssc.configureBlocking(false);
+    public static void main(String[] args) throws Exception {
 
-            Selector selector = Selector.open();
-            ssc.register(selector, SelectionKey.OP_ACCEPT);
+        ServerSocketChannel ssc = ServerSocketChannel.open();
+        ssc.bind(new InetSocketAddress(ADDRESS, PORT));
+        ssc.configureBlocking(false);
 
-            while (true) {
-                int selectNum = selector.select();
+        Selector selector = Selector.open();
+        ssc.register(selector, SelectionKey.OP_ACCEPT);
 
-                if (selectNum == 0) {
-                    Thread.sleep(1000);
-                    continue;
-                }
-                Set<SelectionKey> selectKeys = selector.keys();
-
-                Iterator<SelectionKey> selectionKeyIterable = selectKeys.iterator();
-
-                while (selectionKeyIterable.hasNext()) {
-                    SelectionKey key = selectionKeyIterable.next();
-                    if (key.isValid() && key.isAcceptable()) {
-                        System.out.println("client has accrpt " + Thread.currentThread().getName());
-                        ServerSocketChannel inSsc = (ServerSocketChannel) key.channel();
-                        SocketChannel inSS = inSsc.accept();
-                        inSS.register(selector, SelectionKey.OP_READ);
-
-                    } else if (key.isValid() && key.isReadable()) {
-                        System.out.println("client has readable " + Thread.currentThread().getName());
-                        SocketChannel inSc = (SocketChannel) key.channel();
-                        byteBuffer.clear();
-                        inSc.read(byteBuffer);
-
-                        byteBuffer.flip();
-                        int leng = byteBuffer.limit();
-                        System.out.println(new String(byteBuffer.array(), 0, leng));
-                    }
-                }
+        while (true) {
+            int selectNum = selector.select();
+            if (selectNum == 0) {
+                Thread.sleep(2000);
             }
-        } catch (Exception e) {
-            System.out.println(e);
+            Set<SelectionKey> selectKeys = selector.selectedKeys();
+
+            Iterator<SelectionKey> selectionKeyIterable = selectKeys.iterator();
+
+            while (selectionKeyIterable.hasNext()) {
+
+                SelectionKey key = selectionKeyIterable.next();
+
+                if (key.isValid() && key.isAcceptable()) {
+                    System.out.println("client has accrpt " + Thread.currentThread().getName());
+                    ServerSocketChannel inSsc = (ServerSocketChannel) key.channel();
+                    SocketChannel inSS = inSsc.accept();
+                    inSS.configureBlocking(false);
+                    inSS.register(selector, SelectionKey.OP_READ);
+
+                } else if (key.isValid() && key.isReadable()) {
+                    System.out.println("client has readable " + Thread.currentThread().getName());
+                    SocketChannel inSc = (SocketChannel) key.channel();
+                    byteBuffer.clear();
+                    inSc.read(byteBuffer);
+
+                    byteBuffer.flip();
+                    System.out.println(byteBuffer.limit());
+                    inSc.register(selector, SelectionKey.OP_WRITE);
+                }else if(key.isWritable()){
+                    SocketChannel inSc = (SocketChannel) key.channel();
+                    byteBuffer.clear();
+                    byteBuffer.put("dabaichi".getBytes());
+                    byteBuffer.flip();
+                    inSc.write(byteBuffer);
+                    inSc.register(selector, SelectionKey.OP_READ);
+                }
+                selectionKeyIterable.remove();
+            }
         }
+
     }
 }
