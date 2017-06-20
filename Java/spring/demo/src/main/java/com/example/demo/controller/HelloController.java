051c0c4afc75ca505d33b767e36fceff663ef66b
cococa <cocoooooa@gmail.com>
2017-06-20 21:20:32 +0800

    add spring-boot mysql operator

--- Java/spring/demo/src/main/java/com/example/demo/controller/HelloController.java
+++ Java/spring/demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,14 +1,11 @@
-package com.example.demo;
+package com.example.demo.controller;
 
-import com.google.gson.Gson;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
+import com.example.demo.bean.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
