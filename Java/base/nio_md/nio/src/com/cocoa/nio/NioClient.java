e3573e5a981092d92bfc0742636906f7bc6eb8ec
cococa <cocoooooa@gmail.com>
2017-07-01 16:13:30 +0800

    add all

--- Java/base/nio_md/nio/src/com/cocoa/nio/NioClient.java
+++ Java/base/nio_md/nio/src/com/cocoa/nio/NioClient.java
@@ -5,10 +5,7 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.InetSocketAddress;
 import java.net.Socket;
-<<<<<<< HEAD
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-=======
+
 import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
@@ -16,7 +13,6 @@ import java.nio.channels.SocketChannel;
 import java.util.Iterator;
 import java.util.Scanner;
 import java.util.Set;
->>>>>>> 500b8c228860b665ceb19f293284d900c6708d34
 
 /**
  * Created by cocoa on 2017/6/14.
@@ -31,62 +27,11 @@ public class NioClient extends Main {
 
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-//        try {
-//            Socket socket = new Socket(ADDRESS, PORT);
-//            OutputStream outputStream = socket.getOutputStream();
-//            OutputStreamWriter bufferedOutputStream = new OutputStreamWriter(outputStream);
-//            bufferedOutputStream.write("Hello nio server");
-//            bufferedOutputStream.close();
-//            outputStream.close();
-//            socket.close();
-//        } catch (Exception e) {
-//            System.out.println(e.toString());
-//        }
-
-
-        String ss = ("ashdashjdajhj\n17682342410知道的123");
-
-        System.out.println(ss.split("\\n").length);
-
-    }
-
-    /**
-     * 检测字符串中含有手机号，手机号检测只检测是否是1开头（类似taobao）
-     * @param num
-     * @return
-     */
-    public static boolean havePhoneNum(String num) {
-        Pattern pattern = Pattern.compile("\\d+");
-        Matcher matcher = pattern.matcher(num);
-        while (matcher.find()) {
-            String numStr = matcher.group(0);
-            if (numStr.length() == 11 && numStr.startsWith("1")) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    /**
-     * 包含三个数字
-     * @param num
-     * @return
-     */
-    public static boolean haveThreeNum(String num) {
-        Pattern pattern = Pattern.compile("\\d+");
-        Matcher matcher = pattern.matcher(num);
-        while (matcher.find()) {
-            String numStr = matcher.group(0);
-            if (numStr.length() == 3) {
-                return true;
-            }
-=======
         try {
             ss = SocketChannel.open();
 //            ss.socket().bind(new InetSocketAddress(PORT));
             ss.configureBlocking(false);
-            ss.connect(new InetSocketAddress(ADDRESS,PORT));
+            ss.connect(new InetSocketAddress(ADDRESS, PORT));
             selector = Selector.open();
             ss.register(selector, SelectionKey.OP_CONNECT);
 
@@ -109,11 +54,8 @@ public class NioClient extends Main {
 
         } catch (Exception e) {
             System.out.println(e.toString());
->>>>>>> 500b8c228860b665ceb19f293284d900c6708d34
         }
-        return false;
     }
-
 
     public static void process(String msg) throws Exception {
         while (true) {
