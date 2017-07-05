2255782a27d0e61f3637ca8e3bbb40d50c781699
cococa <cocoooooa@gmail.com>
2017-07-05 22:42:16 +0800

    add some fils

---
+++ iOS/MyPlayground.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var name = "cocoa"
+var s =
+"""
+\(name)
+asdas
+dasdasd
+asdasd
+adasd
+"""
+print(s)
---
+++ iOS/UseNoXib/UseNoXib/ViewController.swift
@@ -0,0 +1,82 @@
+//
+//  ViewController.swift
+//  UseNoXib
+//
+//  Created by cocoa on 2017/7/2.
+//  Copyright © 2017年 cocoa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//    
+//        let view = UIView();
+//        view.backgroundColor = UIColor.black
+//        view.translatesAutoresizingMaskIntoConstraints = false
+//        self.view.addSubview(view)
+//        
+//        let redView = UIView()
+//        redView.backgroundColor = UIColor.red
+//        redView.translatesAutoresizingMaskIntoConstraints = false
+//        self.view.addSubview(redView)
+//        
+//        initRedView(redView);
+//         // 给自身添加宽度和高度约束
+//        // 给父控件添加顶部和右边的距离约束
+//        // 宽度约束
+//        let widthConstraint = NSLayoutConstraint(item: view, attribute: NSLayoutAttribute.width, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 0.0, constant: 100)
+//        view.addConstraint(widthConstraint)
+//        
+//    
+//        //高度约束
+//        let height:NSLayoutConstraint = NSLayoutConstraint(item: view, attribute: NSLayoutAttribute.height, relatedBy:NSLayoutRelation.equal, toItem:nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier:0.0, constant:100)
+//        
+//        view.addConstraint(height)//自己添加约束
+//        
+//        //右边约束
+//        let right:NSLayoutConstraint = NSLayoutConstraint(item: view, attribute: NSLayoutAttribute.right, relatedBy:NSLayoutRelation.equal, toItem:redView, attribute:NSLayoutAttribute.right, multiplier:2.0, constant: 80)
+//        
+//        view.superview?.addConstraint(right)//父控件添加约束
+//        
+//        //下边约束  item1.attribute1 = multiplier × item2.attribute2 + constant
+//
+//        let bottom:NSLayoutConstraint = NSLayoutConstraint(item: view, attribute: NSLayoutAttribute.bottom , relatedBy:NSLayoutRelation.equal, toItem:redView, attribute:NSLayoutAttribute.bottom, multiplier:1.0, constant: 100)
+//        
+//        view.superview?.addConstraint(bottom)//父控件添加约束
+//        
+//        
+//    }
+//
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
+//
+//    
+//    func initRedView(_ redView : UIView){
+//        let width = NSLayoutConstraint(item: redView, attribute: NSLayoutAttribute.width, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 0.0, constant: 20)
+//        
+//        redView.addConstraint(width)
+//        
+//        let height = NSLayoutConstraint(item: redView, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 0.0, constant: 20)
+//        
+//        redView.addConstraint(height)
+//        
+//        //右边约束
+//        let right:NSLayoutConstraint = NSLayoutConstraint(item: redView, attribute: NSLayoutAttribute.left, relatedBy:NSLayoutRelation.equal, toItem:self.view, attribute:NSLayoutAttribute.left, multiplier:1.0, constant: 20)
+//        
+//        redView.superview!.addConstraint(right)//父控件添加约束
+//        
+//        //下边约束
+//        let bottom:NSLayoutConstraint = NSLayoutConstraint(item: redView, attribute: NSLayoutAttribute.top, relatedBy:NSLayoutRelation.equal, toItem:self.view, attribute:NSLayoutAttribute.top, multiplier:1.0, constant: 20)
+//        
+//        redView.superview!.addConstraint(bottom)//父控件添加约束
+    }
+    
+    
+
+}
+
