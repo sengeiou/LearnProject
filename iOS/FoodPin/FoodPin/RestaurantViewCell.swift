2b24e6ae8760a3e9d3e48bfba267f7d5f258a998
cococa <cocoooooa@gmail.com>
2017-07-08 16:21:28 +0800

    add customer tableview

---
+++ iOS/FoodPin/FoodPin/RestaurantViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  ReataurantViewCell.swift
+//  FoodPin
+//
+//  Created by cocoa on 2017/7/8.
+//  Copyright © 2017年 cocoa. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantViewCell: UITableViewCell {
+    
+    @IBOutlet weak var img: UIImageView!
+    
+    @IBOutlet weak var name: UILabel!
+    
+    @IBOutlet weak var location: UILabel!
+    
+    @IBOutlet weak var type: UILabel!
+}
