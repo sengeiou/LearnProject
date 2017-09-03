c40f55d7220a8d76cf9ec3c0516e7bdd6b865faa
cococa <cocoooooa@gmail.com>
2017-09-03 21:54:41 +0800

    add iOS textfield

--- MacOS/base/MyPlayground.playground/Contents.swift
+++ MacOS/base/MyPlayground.playground/Contents.swift
@@ -5,6 +5,26 @@
 import UIKit
 
 
+func greet( age: Int) {
+    guard   else {
+        return
+    }
+}
+
+
+
+var  minMath : Int  = 1
+minMath = +1
+print(minMath)
+
+for item in stride(from: 0, to: 3, by: 1){
+    print("the item in \(item)")
+}
+
+for a in stride(from: 0, to: 100, by: 10){
+    print(a)
+}
+
 var button = UILabel(frame: CGRect(x: 100, y: 100, width: 200, height: 50))
 
     button.text = "cocoa"
