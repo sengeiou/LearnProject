529c089018de81222db496be7e738ee65a9108e9
cococa <cocoooooa@gmail.com>
2017-11-15 21:31:47 +0800

    add code constriant

--- iOS/iOS Programming/Chapter6/Chapter6/ViewController.swift
+++ iOS/iOS Programming/Chapter6/Chapter6/ViewController.swift
@@ -22,10 +22,35 @@ class ViewController: UIViewController {
         segmentControl.translatesAutoresizingMaskIntoConstraints = false
         self.view.addSubview(segmentControl)
         
+       let margins = view.layoutMarginsGuide
         
+       let topConstraint = segmentControl.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor)
+       let leadingConstraint = segmentControl.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
+       let trailinConstraint = segmentControl.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+        
+        topConstraint .isActive = true
+            leadingConstraint.isActive = true
+        trailinConstraint.isActive = true
+        
+        segmentControl.addTarget(self, action: #selector(typeChanged(seg:)), for: .valueChanged)
+    }
+    
+    @objc func typeChanged(seg: UISegmentedControl){
+        switch seg.selectedSegmentIndex {
+        case 0:
+            print("0")
+        case 1:
+            print("1")
+        case 2:
+            print("2")
+        default:
+            print("default")
+        }
         
         
     }
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
