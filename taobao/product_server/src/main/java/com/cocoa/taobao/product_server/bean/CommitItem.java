19f4cb59e533f1e37bd90775cf75d036cc28260b
junshen <shenjun@kuaiqiangche.com>
2017-11-22 20:01:41 +0800

    add something

--- taobao/product_server/src/main/java/com/cocoa/taobao/product_server/bean/CommitItem.java
+++ taobao/product_server/src/main/java/com/cocoa/taobao/product_server/bean/CommitItem.java
@@ -16,7 +16,7 @@ import java.util.Date;
  * @date: 17/8/23 11:49
  */
 //CREATE TABLE `taobao_item` (
-//        `id` int(11) NOT NULL,
+//        `id` int(11) NOT NULL AUTO_INCREMENT,
 //        `item_url` varchar(120) COLLATE utf8_bin DEFAULT NULL,
 //        `nick` varchar(80) COLLATE utf8_bin DEFAULT NULL,
 //        `num_iid` varchar(45) COLLATE utf8_bin DEFAULT NULL,
@@ -33,8 +33,9 @@ import java.util.Date;
 //        `sales_update_time` datetime DEFAULT NULL,
 //        `create_time` datetime DEFAULT NULL,
 //        `item_id` varchar(45) COLLATE utf8_bin DEFAULT NULL,
+//        `status` int(11) NOT NULL DEFAULT '0',
 //        PRIMARY KEY (`id`)
-//        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+//        ) ENGINE=InnoDB AUTO_INCREMENT=10580 DEFAULT CHARSET=utf8 COLLATE=utf8_bin
 
 
 @Table(name = "taobao_item")
