8e0618848cfcbbc1127a24c0c36a04b32fc8cf24
cococa <cocoooooa@gmail.com>
2018-01-10 15:50:46 +0800

    add shiji tools

---
+++ project/shiji/Shiji/app/src/main/java/com/cococa/shiji/ShiApplication.java
@@ -0,0 +1,35 @@
+package com.cococa.shiji;
+
+import android.app.Application;
+
+/**
+ * 在此写用途
+ *
+ * @version V1.0 <描述当前版本功能>
+ * @FileName: com.cococa.shiji.ShiApplication
+ * @author: shenjun@kuaiqiangche.com
+ * @date: 17/7/3 20:54
+ */
+public class ShiApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+//        AlibcTradeSDK.asyncInit(this, new AlibcTradeInitCallback() {
+//            @Override
+//            public void onSuccess() {
+//                //初始化成功，设置相关的全局配置参数
+//                // ...
+//                Log.e("----", "success");
+//            }
+//
+//            @Override
+//            public void onFailure(int code, String msg) {
+//                //初始化失败，可以根据code和msg判断失败原因，详情参见错误说明
+//                Log.e("----", code + "--faile--" + msg);
+//
+//            }
+//        });
+
+    }
+}
---
+++ project/shiji/Shiji/app/src/main/java/com/cococa/shiji/resp_bean/RespTBKItem.java
@@ -0,0 +1,17 @@
+package com.cococa.shiji.resp_bean;
+
+/**
+ * Created by sj on 17/8/23.
+ */
+public class RespTBKItem {
+
+    private RespTBKResult results;
+
+    public RespTBKResult getResults() {
+        return results;
+    }
+
+    public void setResults(RespTBKResult results) {
+        this.results = results;
+    }
+}
