2956531f3a750c515ef7828aa6d612d3d744aefd
cocoooooa <cocoooooa@gmail.com>
2019-07-24 21:43:13 +0800

    add es and vue router

---
+++ JavaScript/VueTest/src/view/page1.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="app">
+    <h1>page1</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "page2",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  },
+  created: function() {
+    console.log(letapp.name)
+    console.log(constapp.name)
+    console.log(this.test1() +" // 可以看到let是在块级作用域中") 
+  },
+  methods: {
+     test1() {
+      let aaa = 10
+      if(aaa > 0) {
+        let aaa = 11111
+      }
+      return aaa
+    }
+  }
+}
+let letapp = {name :'let'}
+const constapp = {name:"const"}
+
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ JavaScript/VueTest/src/view/page2.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="app">
+    <h1>page2</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "page2",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  },
+  created: function() {
+  },
+  methods: {
+    xxx( x = 22, y = 10) {
+      return x + y
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
