c3dd71cf31b85308aedf60884f21b5035e650157
cococa <cocoooooa@gmail.com>
2019-11-05 16:34:40 +0800

    add dart

--- flutter/dart/base1.dart
+++ flutter/dart/base1.dart
@@ -1,16 +1,40 @@
-void main(){
-    var name = "bob";
-    print(name);
+void main() {
+  // int
 
-    int count ;
+  var i1 = 19.00;
+  print("the i1 abs() = ${i1.abs()}");
 
-    print("未初始化的变量具有 null 的初始值。即使数字类型变量最初为 null ，因为数字是对象。$count");
+  i1.toString();
+  print("the i1 isNaN = ${i1.isNaN}");
+  print("the i1 isNegative = ${i1.isNegative}");
+  print("the i1 sign = ${i1.sign}");
+  print("the i1 runtimeType = ${i1.runtimeType}");
 
-    print("${1+2}");
+  var hashCode = i1.hashCode;
+  print("the hashcode is $hashCode");
 
-    final  age = 12;
-    const p1 = "123";
+  var address  = "zhejiang";
+  print("address hashcode is ${address.hashCode}");
 
+  var a2 = int.parse("10");  
+  double d2 = double .parse("00.99-");
+
+
+  print("the a2 is ${a2}");
+  print("the d2 is ${d2}");
+
+
+  // int i2 = 10.00;  // double 类型不能赋值int  会报错
+
+  var name = "bob";
+  print(name);
+
+  int count;
+
+  print("未初始化的变量具有 null 的初始值。即使数字类型变量最初为 null ，因为数字是对象。$count");
+
+  print("${1 + 2}");
+
+  final age = 12;
+  const p1 = "123";
 }
-
-
