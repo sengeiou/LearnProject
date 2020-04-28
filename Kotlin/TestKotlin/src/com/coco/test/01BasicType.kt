393d378a3f606527f20f43df038cde6f50ad8713
cococa <cocoooooa@gmail.com>
2020-04-28 11:38:11 +0800

    add kt

---
+++ Kotlin/TestKotlin/src/com/coco/test/01BasicType.kt
@@ -0,0 +1,12 @@
+package com.coco.test
+
+
+val one = 1
+var two: Int = 2
+
+
+fun main() {
+//        println("the type of one ${typeOf<one>()}")
+
+
+}
---
+++ Kotlin/TestKotlin/src/com/coco/test/05Classes.kt
@@ -0,0 +1,87 @@
+package com.coco.test
+
+
+open class Base(val name : String){
+    init {
+        println("Initializing Base")
+    }
+
+    open val size : Int = name .length .also {
+        println("Initializing size in Base : ${it}")
+    }
+}
+
+
+class Derived (name : String , val lastName : String)
+    :Base(name.capitalize().also { println("Argument for base ${it}") }){
+
+    init {
+        println("Initializing Derived")
+    }
+
+    override val size: Int
+        = (super.size + lastName.length).also { println("Initializing size in Derived ${it}") }
+}
+
+
+
+
+
+
+
+
+fun main() {
+    var p = Person("cocoa")
+    println(p)
+//        val p1 = Person(1)
+//    派生类初始化顺序
+    println("Constructing Derived(\"hello\", \"world\")")
+    val d = Derived("hello", "world")
+
+
+}
+
+class Empty
+
+class Invoice {
+
+}
+
+
+class Person constructor(name: String) {
+
+    var customerKey = name
+
+    init {
+        println("this is person init ${name}--${customerKey}")
+    }
+
+//    constructor( age : Int){
+////        customerKey.length.also {  }
+//    }
+
+}
+
+
+open class Parent {
+
+    open val count: Int = 10
+
+    open fun print() {
+        println("this is print from parent")
+    }
+}
+
+class Child : Parent() {
+
+    override var count: Int = 0
+        get() = super.count
+
+
+    override fun print() {
+        super.print()
+        println("this is print from parent")
+    }
+
+}
+
