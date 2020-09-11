fe1113e01c1161156e117f572cbdb32fa1c720c6
cococa <cocoooooa@gmail.com>
2020-09-11 18:00:07 +0800

    feat [add kt]

---
+++ Kotlin/TestKT/src/com/coco/test/collections/4Ranges.kt
@@ -0,0 +1,22 @@
+package com.coco.test.collections
+
+fun main(){
+
+    for(i in 1..3){
+        println("${i}")
+    }
+
+    // 反向迭代
+
+    for (i in 9 downTo 2){
+        println("${i}")
+    }
+
+    for (i in 1 until 10){
+        println("util ${i}")
+    }
+
+
+
+
+}
