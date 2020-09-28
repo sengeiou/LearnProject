47a36cf4f1628c35af7b641b2b5670403d5daf22
cococa <cocoooooa@gmail.com>
2020-09-28 21:05:07 +0800

    feat [add cmake test1]

---
+++ C/Cmake/test1/CMakeFiles/Test1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Test1.dir/main.c.o"
+  "Test1.pdb"
+  "Test1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Test1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
