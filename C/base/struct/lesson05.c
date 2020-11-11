c7b5b725d6150b504b93214d7e986f48d3f36497
cococa <cocoooooa@gmail.com>
2020-11-11 17:27:04 +0800

    add c

--- C/base/struct/lesson05.c
+++ C/base/struct/lesson05.c
@@ -1,18 +1,38 @@
 #include <stdio.h>
-
+#include <string.h>
 // 结构体自引用
 struct Vedio{
    int age;
 //    struct Vedio v;    //会报错, 无法确定v 的大小
     struct Vedio* v;    // 使用指针可以确定长度
-
-
 };
 
 
+struct Person {
+    int age;
+    char name[10];
+};
+
 int main(){
 
+
+    struct Person p = {12,"123"};
+    struct Person *p1 = &p;
+    int *a = &p1-> age;  // 箭头符号的优先级   
+
+    *a = 11000;
+
     
+    char *dest  = "thisisnameAndroidFlutterIOS";
+    size_t len =  strlen(dest);
+    memcpy(p1->name,dest,len);
+
+
+    printf("print name of person %d\n", p1->age);
+    printf("print name of person %s\n", p1->name);
+
+
+
     return 0;
 }
 
