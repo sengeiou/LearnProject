b3b82dba5056aef33ea3e0717e3c2c4f0579d7bf
cococa <cocoooooa@gmail.com>
2020-11-19 11:09:45 +0800

    a

--- swift/LinuxSwift/Closures/Sources/Closures/main.swift
+++ swift/LinuxSwift/Closures/Sources/Closures/main.swift
@@ -1,1 +1,38 @@
-print("Hello, world!")
+
+
+func sorted(_ s1 : String, _ s2 : String ) -> Bool {
+    return s1 < s2
+}
+
+
+// example1
+let names = ["b1","a2","c2","v3"]
+let sortedList = names.sorted(by : sorted)
+print("\(sortedList)")
+
+
+// example2
+let sortedList2 = names.sorted(by : {(s1 : String, s2 :String) -> Bool in
+    return s1 < s2  
+})
+print("\(sortedList2)")
+
+
+// example3
+let sortedList3  = names.sorted(by :  {(s1 , s2 ) -> Bool in 
+    return s1 < s2
+})
+print("\(sortedList3)")
+
+
+
+// example4
+let sortedList4 = names.sorted(by : {s1 , s2  in  s1 < s2})
+print("\(sortedList4)")
+
+
+// example5
+let sortedList5  = names.sorted(by : { $0 < $1})
+print("\(sortedList5)")
+
+
