91aaef076b7f4ddd1579c8a7529a2755d4774dd9
cococa <cocoooooa@gmail.com>
2020-11-25 15:35:49 +0800

    update [dart async]

--- flutter/dart/async.dart
+++ flutter/dart/async.dart
@@ -1,5 +1,6 @@
 
-
+// import 'dart:asnyc';
+// import 'dart:html';
 
 
 Future<String> lookupVersion() async{
@@ -8,56 +9,67 @@ Future<String> lookupVersion() async{
     });
 }
 
-Future  checkVersion() async{
-    await lookupVersion()
+// 两种方式获取 lookupVersion ,   future  和   async+await
+void checkVersion1(){
+    lookupVersion()
       .then((value) => print("${value}"))
       .catchError((exception)=>{print("${exception}")});
 }
+//await关键字必须在async函数内部使用
+// 调用async函数必须使用await关键字
+void checkVersion2() async{
+    String name  = await lookupVersion();
+    print("$name");
+}
+
+
 
 // 创建stream
 
 //https://blog.csdn.net/yingshukun/article/details/100855253
-void main(){
-    var list = [1,2,3,4];
-    var stream = Stream.fromIterable(list);
-    var stream1 = Stream.fromIterable(list);
-    var stream2 = Stream.fromIterable(list);
-    var streamFromFuture = Stream.fromFuture(lookupVersion());
-    var streamFromValue = Stream.value(1);
-    var streamEmpty = Stream.empty();
+void main() async{
+    // var list = [1,2,3,4];
+    // var stream = Stream.fromIterable(list);
+    // var stream1 = Stream.fromIterable(list);
+    // var stream2 = Stream.fromIterable(list);
+    // var streamFromFuture = Stream.fromFuture(lookupVersion());
+    // var streamFromValue = Stream.value(1);
+    // var streamEmpty = Stream.empty();
     
-    print("$stream");
-    print("$streamFromFuture");
-    print("$streamFromValue");
-    print("$streamEmpty");
+    // print("$stream");
+    // print("$streamFromFuture");
+    // print("$streamFromValue");
+    // print("$streamEmpty");
 
-    // 监听   forEach/listen
-    // stream.forEach((element) {
-    //     print("$element");
-    // });
+    // // 监听   forEach/listen
+    // // stream.forEach((element) {
+    // //     print("$element");
+    // // });
 
-    stream.listen((event) {
-        print("$event");
-    },
-    onError: (e){ print("error---${e}");},
-    onDone:(){ print("done");}
-    );
-
-
-    // 限制操作    限制数量take(num)   根据表达式显示takeWhile()
-
-    stream1 =  stream1.takeWhile((element)  =>  element < 3);
+    // stream.listen((event) {
+    //     print("$event");
+    // },
+    // onError: (e){ print("error---${e}");},
+    // onDone:(){ print("done");}
+    // );
 
 
-    // 跳过   skip   skipWhile()
+    // // 限制操作    限制数量take(num)   根据表达式显示takeWhile()
 
-    //Future<List<T>> toList() 表示将Stream中所有数据存储在List中
-    var sList = stream2.map((event) => event *event).toList();
-    sList.then((value) => print("${value}"));
+    // stream1 =  stream1.takeWhile((element)  =>  element < 3);
 
 
+    // // 跳过   skip   skipWhile()
+
+    // //Future<List<T>> toList() 表示将Stream中所有数据存储在List中
+    // var sList = stream2.map((event) => event *event).toList();
+    // sList.then((value) => print("${value}"));
 
 
+    // StreamController streamController = StreamController();
 
-    checkVersion();
+
+    checkVersion1();
+    checkVersion2();
+
 }
--- flutter/flutter_app/lib/TestFutureBuilder.dart
+++ flutter/flutter_app/lib/TestFutureBuilder.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:shared_preferences/shared_preferences.dart'
 
 class TestFutureBuilder extends StatelessWidget {
   @override
